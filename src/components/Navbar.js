d581e4d4c06f67b93c4075728f2b95529f4507ac
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-11-15 23:53:02 +0530

    add: navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "../styles/Navbar.css";
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <div className="navbar__left">
+        <div className="navbar__left-logo">Logo</div>
+        <ul className="navbar__left-list">
+          <li className="navbar__left-list-item">Genre</li>
+          <li className="navbar__left-list-item">Wallpapers</li>
+          <li className="navbar__left-list-item">Manga</li>
+        </ul>
+      </div>
+      <div className="navbar__search">
+        <input type="text" className="navbar__search-input" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
