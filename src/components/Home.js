6823dcde2b0399b7eaeb34cbc96cdd2f6186b619
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-11-19 23:32:30 +0530

    update: Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,30 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "../styles/Home.css";
+import { getAllAnimes } from "../utils/APIHelper";
 import Card from "./Card";
 const Home = () => {
   const [allAnimesData, setAllAnimesData] = useState([]);
   useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://anime-db.p.rapidapi.com/anime",
-      params: {
-        page: "1",
-        size: "20",
-        // search: "Fullmetal",
-        // genres: "Fantasy,Drama",
-        // sortBy: "ranking",
-        // sortOrder: "asc",
-      },
-      headers: {
-        "X-RapidAPI-Key": "f294a746c3mshcebf8b9885eb1eep1c6a56jsn984b7716730c",
-        "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data.data);
-        setAllAnimesData(response.data.data);
-        return response.data;
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    getData();
   }, []);
+  const getData = async () => {
+    let response = await getAllAnimes();
+
+    if (response.status == "success") {
+      console.log({ response });
+      setAllAnimesData(response.data.data);
+    } else if (response.status == "error") {
+      console.log({ response });
+      alert(response.data.message);
+    }
+  };
 
   return (
     <div className="home">
-      {allAnimesData.map(({ title, image }) => (
-        <Card title={title} image={image} />
-      ))}
+      {allAnimesData.length > 0 &&
+        allAnimesData.map(({ title, image }) => (
+          <Card title={title} image={image} />
+        ))}
     </div>
   );
 };
