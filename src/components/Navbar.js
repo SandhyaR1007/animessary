0ff92413673bd99e4de5ec434a9c21c19ca01362
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-11-19 23:32:55 +0530

    add: scroll effect in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Navbar.css";
 const Navbar = () => {
+  const [bgColor, setBgColor] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 90) {
+        setBgColor(true);
+      } else {
+        setBgColor(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${bgColor ? "navbar__black" : ""}`}>
       <div className="navbar__left">
         <div className="navbar__left-logo">Logo</div>
         <ul className="navbar__left-list">
