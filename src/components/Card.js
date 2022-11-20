93a400b979f7ac03f525f401fb1ca7c669111d85
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-11-20 13:30:27 +0530

    add: play icon in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,13 @@ import "../styles/Card.css";
 const Card = ({ title, image }) => {
   return (
     <div className="card">
-      <img src={image} alt={title} />
-      <p>{title}</p>
+      <img src={image} alt={title} className="card__img" />
+      <div className="card__overlay">
+        <div className="card__content">
+          <i className="fa-solid fa-circle-play card__content-playBtn"></i>
+          <p className="card__content-title">{title}</p>
+        </div>
+      </div>
     </div>
   );
 };
