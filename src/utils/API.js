a626caf7ec79b44dc2f0e9fe229d67e2cdf93a5f
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-12-11 21:28:02 +0530

    add: APIs for getGenre

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,4 @@
 export const GET_ALL_ANIMES = "https://anime-db.p.rapidapi.com/anime";
 export const GET_ONE_ANIME_BY_RANKING =
   "https://anime-db.p.rapidapi.com/anime/by-ranking/";
+export const GET_ALL_GENRES = "https://anime-db.p.rapidapi.com/genre";
--- src/utils/APIHelper.js
+++ src/utils/APIHelper.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-import { GET_ALL_ANIMES, GET_ONE_ANIME_BY_RANKING } from "./API";
+import {
+  GET_ALL_ANIMES,
+  GET_ALL_GENRES,
+  GET_ONE_ANIME_BY_RANKING,
+} from "./API";
 const REACT_APP_API_KEY = "f294a746c3mshcebf8b9885eb1eep1c6a56jsn984b7716730c";
 const REACT_APP_API_HOST = "anime-db.p.rapidapi.com";
 const headers = {
@@ -35,6 +39,54 @@ export const getOneAnimeByRanking = async () => {
   const options = {
     method: "GET",
     url: GET_ONE_ANIME_BY_RANKING + "1",
+    headers: headers,
+  };
+  let apiData = "";
+  await axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      apiData = { status: "success", data: response.data };
+      return { status: "success", data: response.data };
+    })
+    .catch(function (error) {
+      console.error(error);
+      apiData = { status: "error", data: error };
+      return { status: "error", data: error };
+    });
+  return apiData;
+};
+export const getAnimeByGenre = async (genre, size) => {
+  const options = {
+    method: "GET",
+    url: GET_ALL_ANIMES,
+    params: {
+      page: "1",
+      size: size,
+      genres: genre,
+    },
+    headers: headers,
+  };
+  let apiData = "";
+  await axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      apiData = { status: "success", data: response.data };
+      return { status: "success", data: response.data };
+    })
+    .catch(function (error) {
+      console.error(error);
+      apiData = { status: "error", data: error };
+      return { status: "error", data: error };
+    });
+  return apiData;
+};
+
+export const getAllGenre = async () => {
+  const options = {
+    method: "GET",
+    url: GET_ALL_GENRES,
     headers: headers,
   };
   let apiData = "";
