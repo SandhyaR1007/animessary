241a9f9383e1cceee589fbf6f7edd35275ba5a97
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-12-11 21:28:23 +0530

    update: Navbar for genre listing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import GenreList from "./GenreList";
 const Navbar = () => {
   const [bgColor, setBgColor] = useState(false);
+  const [isShown, setIsShown] = useState(false);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 90) {
@@ -19,11 +22,24 @@ const Navbar = () => {
   return (
     <nav className={`navbar ${bgColor ? "navbar__black" : ""}`}>
       <div className="navbar__left">
-        <div className="navbar__left-logo">Logo</div>
+        <Link to="/" className="navbar__left-logo">
+          Logo
+        </Link>
         <ul className="navbar__left-list">
-          <li className="navbar__left-list-item">Genre</li>
-          <li className="navbar__left-list-item">Wallpapers</li>
-          <li className="navbar__left-list-item">Manga</li>
+          <li
+            className="navbar__left-list-item"
+            to="/genre"
+            onMouseEnter={() => setIsShown(true)}
+          >
+            Genre
+          </li>
+          {isShown && <GenreList isShown={isShown} setIsShown={setIsShown} />}
+          <Link to="/wallpapers" className="navbar__left-list-item">
+            Wallpapers
+          </Link>
+          <Link to="/manga" className="navbar__left-list-item">
+            Manga
+          </Link>
         </ul>
       </div>
       <div className="navbar__search">
