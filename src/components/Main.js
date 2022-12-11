2aaf71e4443c103cb5cfcd93513968ebeb5ed7eb
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-12-11 21:29:30 +0530

    add: Main component for displaying all lists

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./Card";
+
+const Main = ({ heading, dataList }) => {
+  return (
+    <div className="home">
+      <h3>{heading}</h3>
+      <div className="home__animeList">
+        {dataList.length > 0 &&
+          dataList.map(({ _id, title, image }) => (
+            <Card key={_id} title={title} image={image} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
