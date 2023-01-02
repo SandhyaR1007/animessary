571b4417142be2ae22e28d669063936694a1b9be
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-01-02 22:47:47 +0530

    styles: media queries for screen size less than 700px

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getAnimeById } from "../utils/APIHelper";
+import "../styles/Description.css";
 
 const Description = () => {
   const params = useParams();
@@ -23,13 +24,51 @@ const Description = () => {
   return descriptionData == null ? (
     <></>
   ) : (
-    <div>
-      <h1>{descriptionData.title}</h1>
-      <img src={descriptionData.image} alt={descriptionData.title} />
-      <h3>Episodes</h3>
-      <p>{descriptionData.episodes}</p>
-      <h3>Genre:</h3>
-      <p></p>
+    <div className="description">
+      <div className="description__left">
+        <img
+          className="description__left-img"
+          src={descriptionData.image}
+          alt={descriptionData.title}
+        />
+        <span className="description__left-tag">{descriptionData.type}</span>
+        <div>
+          <span>Episodes: </span>
+          <span>{descriptionData.episodes}</span>
+        </div>
+        <div>
+          <span>Ranking: </span>
+          <span>{descriptionData.ranking}</span>
+        </div>
+      </div>
+      <div className="description__right">
+        <h1 className="description__right-title">{descriptionData.title}</h1>
+        <h4 className="description__right-alter">
+          {descriptionData.alternativeTitles[0]}
+        </h4>
+        <p className="description__right-synopsis">
+          {descriptionData.synopsis.slice(0, 300) + "..."}
+        </p>
+        <div className="description__info">
+          <span className="description__info-head">Type: </span>
+          <span className="description__info-value">
+            {descriptionData.type}
+          </span>
+        </div>
+        <div className="description__info">
+          <span className="description__info-head">Status: </span>
+          <span className="description__info-value">
+            {descriptionData.status}
+          </span>
+        </div>
+        <div className="desciption__right-genres">
+          {descriptionData.genres.map((item, index) => (
+            <span className="desciption__right-genres__bullet" key={index}>
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
